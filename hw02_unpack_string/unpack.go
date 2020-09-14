1a3d3beace485a57bcc4870266db19ffc99ea3af
Oleg Larionov <larionovog8@gmail.com>
2020-09-14 20:28:38 +0300

    HW2 is completed (#3)
    * HW2 is completed
    
    * Упростил реализацию

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,68 @@ package hw02_unpack_string //nolint:golint,stylecheck
 
 import (
 	"errors"
+	"strconv"
+	"strings"
+	"unicode"
+)
+
+const (
+	beginState = iota
+	letterState
+	escapeState
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here
-	return "", nil
+func Unpack(input string) (string, error) {
+	builder := &strings.Builder{}
+
+	state := beginState
+	var prevSymbol, curSymbol rune
+	for _, curSymbol = range input {
+		switch state {
+		case beginState:
+			if unicode.IsDigit(curSymbol) {
+				return "", ErrInvalidString
+			}
+			if curSymbol == '\\' {
+				state = escapeState
+				break
+			}
+
+			prevSymbol = curSymbol
+			state = letterState
+
+		case letterState:
+			if unicode.IsDigit(curSymbol) {
+				n, _ := strconv.Atoi(string(curSymbol))
+				value := strings.Repeat(string(prevSymbol), n)
+				builder.WriteString(value)
+				state = beginState
+				break
+			}
+
+			builder.WriteRune(prevSymbol)
+			if curSymbol == '\\' {
+				state = escapeState
+			}
+			prevSymbol = curSymbol
+
+		case escapeState:
+			if curSymbol == '\\' || unicode.IsDigit(curSymbol) {
+				prevSymbol = curSymbol
+				state = letterState
+				break
+			}
+			return "", ErrInvalidString
+		}
+	}
+	if state == escapeState {
+		return "", ErrInvalidString
+	}
+	if state == letterState {
+		builder.WriteRune(curSymbol)
+	}
+
+	return builder.String(), nil
 }
--- hw02_unpack_string/unpack_test.go
+++ hw02_unpack_string/unpack_test.go
@@ -45,6 +45,22 @@ func TestUnpack(t *testing.T) {
 			input:    "aaa0b",
 			expected: "aab",
 		},
+		{
+			input:    "\tabc\n",
+			expected: "\tabc\n",
+		},
+		{
+			input:    "\t0a1b2c3\n4",
+			expected: "abbccc\n\n\n\n",
+		},
+		{
+			input:    "走吧",
+			expected: "走吧",
+		},
+		{
+			input:    "走2吧3",
+			expected: "走走吧吧吧",
+		},
 	} {
 		result, err := Unpack(tst.input)
 		require.Equal(t, tst.err, err)
@@ -53,8 +69,6 @@ func TestUnpack(t *testing.T) {
 }
 
 func TestUnpackWithEscape(t *testing.T) {
-	t.Skip() // Remove if task with asterisk completed
-
 	for _, tst := range [...]test{
 		{
 			input:    `qwe\4\5`,
@@ -71,6 +85,41 @@ func TestUnpackWithEscape(t *testing.T) {
 		{
 			input:    `qwe\\\3`,
 			expected: `qwe\3`,
+		},
+		{
+			input:    `\`,
+			expected: ``,
+			err:      ErrInvalidString,
+		},
+		{
+			input:    `\1`,
+			expected: `1`,
+		},
+		{
+			input:    `\10`,
+			expected: ``,
+		},
+		{
+			input:    `q\we`,
+			expected: ``,
+			err:      ErrInvalidString,
+		},
+		{
+			input:    `q\\we`,
+			expected: `q\we`,
+		},
+		{
+			input:    `qwe\4\5\`,
+			expected: ``,
+			err:      ErrInvalidString,
+		},
+		{
+			input:    `\1qwe`,
+			expected: `1qwe`,
+		},
+		{
+			input:    `走\2吧3`,
+			expected: "走2吧吧吧",
 		},
 	} {
 		result, err := Unpack(tst.input)
