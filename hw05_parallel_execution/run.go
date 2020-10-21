347996cb13bb07823cee23fb85002930a0f1283e
Oleg Larionov <larionovog8@gmail.com>
2020-10-21 18:49:19 +0300

    HW5 is completed (#6)
    * HW5 is completed
    
    * Упростил код

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -2,14 +2,63 @@ package hw05_parallel_execution //nolint:golint,stylecheck
 
 import (
 	"errors"
+	"sync"
+	"sync/atomic"
 )
 
 var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
 
 type Task func() error
 
-// Run starts tasks in N goroutines and stops its work when receiving M errors from tasks
-func Run(tasks []Task, N int, M int) error {
-	// Place your code here
+// Run starts tasks in N goroutines and stops its work when receiving M errors from tasks.
+func Run(tasks []Task, n int, m int) error {
+	if n <= 0 {
+		n = 1
+	}
+	if m <= 0 {
+		m = 0
+	}
+
+	mCasted := int32(m)
+	taskCh := make(chan Task)
+	quitCh := make(chan struct{})
+	var errorsCounter int32 = 0
+
+	wg := sync.WaitGroup{}
+	worker := func(taskCh <-chan Task, quitCh <-chan struct{}) {
+		defer wg.Done()
+		for {
+			select {
+			case <-quitCh:
+				return
+			case task := <-taskCh:
+				err := task()
+				if err != nil {
+					atomic.AddInt32(&errorsCounter, 1)
+				}
+			}
+		}
+	}
+
+	wg.Add(n)
+	for i := 0; i < n; i++ {
+		go worker(taskCh, quitCh)
+	}
+
+	for _, task := range tasks {
+		if mCasted != 0 && atomic.LoadInt32(&errorsCounter) >= mCasted {
+			break
+		}
+		taskCh <- task
+	}
+	close(quitCh)
+
+	wg.Wait()
+	close(taskCh)
+
+	if mCasted != 0 && errorsCounter >= mCasted {
+		return ErrErrorsLimitExceeded
+	}
+
 	return nil
 }
--- hw05_parallel_execution/run_test.go
+++ hw05_parallel_execution/run_test.go
@@ -1,6 +1,7 @@
 package hw05_parallel_execution //nolint:golint,stylecheck
 
 import (
+	"errors"
 	"fmt"
 	"math/rand"
 	"sync/atomic"
@@ -65,5 +66,78 @@ func TestRun(t *testing.T) {
 
 		require.Equal(t, runTasksCount, int32(tasksCount), "not all tasks were completed")
 		require.LessOrEqual(t, int64(elapsedTime), int64(sumTime/2), "tasks were run sequentially?")
+	})
+
+	t.Run("if M=0 then ignore errors", func(t *testing.T) {
+		tasksCount := 50
+		tasks := make([]Task, 0, tasksCount)
+
+		var runTasksCount int32
+
+		for i := 0; i < tasksCount; i++ {
+			err := fmt.Errorf("error from task %d", i)
+			tasks = append(tasks, func() error {
+				time.Sleep(time.Millisecond * time.Duration(rand.Intn(100)))
+				atomic.AddInt32(&runTasksCount, 1)
+				return err
+			})
+		}
+
+		workersCount := 2
+		maxErrorsCount := 0
+		result := Run(tasks, workersCount, maxErrorsCount)
+
+		require.Equal(t, nil, result)
+		require.Equal(t, int32(tasksCount), runTasksCount)
+	})
+
+	t.Run("M=1; N=5; error in last task", func(t *testing.T) {
+		tasksCount := 50
+		tasks := make([]Task, 0, tasksCount)
+
+		var runTasksCount int32
+
+		n := tasksCount - 1
+		for i := 0; i < n; i++ {
+			tasks = append(tasks, func() error {
+				time.Sleep(time.Millisecond * time.Duration(rand.Intn(100)))
+				atomic.AddInt32(&runTasksCount, 1)
+				return nil
+			})
+		}
+		tasks = append(tasks, func() error {
+			atomic.AddInt32(&runTasksCount, 1)
+			return errors.New("error from last task")
+		})
+
+		maxErrorsCount := 1
+		workersCount := 5
+		result := Run(tasks, workersCount, maxErrorsCount)
+
+		require.Equal(t, ErrErrorsLimitExceeded, result)
+		require.GreaterOrEqual(t, runTasksCount, int32(tasksCount-workersCount+1))
+		require.LessOrEqual(t, runTasksCount, int32(tasksCount))
+	})
+
+	t.Run("N > len(tasks)", func(t *testing.T) {
+		tasksCount := 5
+		tasks := make([]Task, 0, tasksCount)
+
+		var runTasksCount int32
+
+		for i := 0; i < tasksCount; i++ {
+			tasks = append(tasks, func() error {
+				time.Sleep(time.Millisecond * time.Duration(rand.Intn(100)))
+				atomic.AddInt32(&runTasksCount, 1)
+				return nil
+			})
+		}
+
+		workersCount := 10
+		maxErrorsCount := 0
+		result := Run(tasks, workersCount, maxErrorsCount)
+
+		require.Equal(t, nil, result)
+		require.Equal(t, int32(tasksCount), runTasksCount)
 	})
 }
